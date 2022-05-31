99068c5bc14763c9132691bd7e08b01912b69f6f
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-05-31 16:51:40 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord("z"), ord("a") - 1, -1):
-    if i % 2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr(i - 32), end="")
+    if i % 2 == 1:
+        i -= 32
+    print("{}".format(chr(i)), end="")
