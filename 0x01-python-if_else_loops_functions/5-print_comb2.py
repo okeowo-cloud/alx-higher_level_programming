68c02b8e0f10792a1cdb36dddff0395795a2b2e1
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-05-31 05:06:13 -0700

    fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print(f"{i}")
+        print("{}".format(i))
     else:
-        print(f"{i:02},", end=" ")
+        print("{},".format(str(i).zfill(2)), end=" ")
