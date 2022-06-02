06cc8f97f30385e174ea6d647fb3efd3adc32e31
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-02 12:56:40 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from 100-my_calculator import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
     import sys
     args = sys.argv
     argc = len(args)
