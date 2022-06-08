740ed1d39e21b2d51124119d934f2f1277574dd4
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-08 15:29:44 -0700

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
     elem_2_mul = [x * y for x, y in my_list]
     elem_2_div = [y for x, y in my_list]
     total, no_of_element = 0.0, 0.0
