bd2ecd4ca57a67c15b81fc912f6784a235f2a251
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-08 08:20:21 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary.keys) == 0:
+    if a_dictionary is None or len(a_dictionary.keys()) == 0:
         return None
     max_score = a_dictionary.get(list(a_dictionary.keys())[0])
     index = list(a_dictionary.keys())[0]
