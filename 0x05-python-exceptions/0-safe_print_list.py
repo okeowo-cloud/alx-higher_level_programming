b5e6939ea17a8bf6c534aeb4bd392fce4df23178
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-20 08:23:23 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     n = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
             n += 1
-        print()
-    except IndexError:
-        print("", end="")
-    finally:
-        return True
+        except IndexError:
+            print("", end="")
+    print()
+    return n
