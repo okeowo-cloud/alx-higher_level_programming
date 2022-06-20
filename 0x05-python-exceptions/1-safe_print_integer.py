6c5bc9daae4ca6dff34e6068b23f52cc6963cfe7
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-20 08:38:10 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+        else:
+            return False
     except ValueError:
         print("", end="")
-        return False
-    else:
-        return True
