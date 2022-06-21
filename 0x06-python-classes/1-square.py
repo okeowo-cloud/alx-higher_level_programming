3865fc078e19dd7e72b7e989592ce37af703bd62
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-21 12:53:11 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square docstring
+
+Module defines a Square with size
+
+"""
+
+
+class Square:
+    """ Represent a square """
+
+    def __init__(self, size):
+        """ Initialize a new square.
+
+        Args:
+        size(int): The size of the new square.
+        """
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -2,3 +2,5 @@ Description:
 
 0-square.py: Is an empty class Square that defines a square
 
+1-square.py: defines a square by: (based on 0-square.py) with private instance variable size.
+
