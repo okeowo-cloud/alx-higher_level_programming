565fa030d15a8ca5ae97227dd40ec3ad759a0d96
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-21 14:31:16 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -16,8 +16,8 @@ class Square:
         size(int): The size of the new square.
 
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
