19924f25c07b03ff29b9142229bd057b5fdca9fa
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-21 12:41:33 -0700

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square docstring.
+
+This module defines a Square
+
+"""
+
+
+class Square:
+    """Represent a square"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,4 @@
+Description:
+
+0-square.py: Is an empty class Square that defines a square
+
