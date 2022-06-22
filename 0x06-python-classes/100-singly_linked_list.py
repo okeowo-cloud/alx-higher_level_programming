3dd32c96512e4701e8c9b8f6d57f6ae8cf4ae4c1
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-22 07:02:57 -0700

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,100 +3,74 @@
 
 
 class Node:
-    """Represents a Node in a SinglyLinkedList"""
+    """Represent a node in a singly-linked list."""
+
     def __init__(self, data, next_node=None):
+        """Initialize a new Node.
+        Args:
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
         """
-        Initializes a Node
-        :param data: is an integer to store in node
-        :param next_node: the next node
-        """
-        self.__data = data
-        self.__next_node = next_node
-
-    @property
-    def next_node(self):
-        """
-        getters for next_node
-        :return: returns the next_node
-        """
-        return self.__next_node
-
-    @next_node.setter
-    def next_node(self, value):
-        """
-        setter for next_node
-        :param value: node
-        :return: Always void.
-
-        """
-        if (value is not None and
-                not isinstance(value, Node)):
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
-        """
-        getter for data in a node
-        :return: an integer data
-
-        """
-        return self.__data
+        """Get/set the data of the Node."""
+        return (self.__data)
 
     @data.setter
     def data(self, value):
-        """
-        Setter for data in a node
-        :param value: is an integer to set as data
-        :return: Always void
-        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
+    @property
+    def next_node(self):
+        """Get/set the next_node of the Node."""
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
 
 class SinglyLinkedList:
-
-    """Represents a SinglyLinkedList"""
+    """Represent a singly-linked list."""
 
     def __init__(self):
-
-        """Initializes a SinglyLinkedList"""
+        """Initalize a new SinglyLinkedList."""
         self.__head = None
 
     def sorted_insert(self, value):
-        """
-        Inserts a new node to the SinglyLinkedList
-
-        The node is inserted into the linkedlist in the
-        correct natural numeric position
-
-        :param value: The new node to be inserted
-        :return: void.
-
+        """Insert a new Node to the SinglyLinkedList.
+        The node is inserted into the list at the correct
+        ordered numerical position.
+        Args:
+            value (Node): The new Node to insert.
         """
         new = Node(value)
         if self.__head is None:
+            new.next_node = None
             self.__head = new
-            self.__head.next_node = None
         elif self.__head.data > value:
             new.next_node = self.__head
             self.__head = new
         else:
             tmp = self.__head
             while (tmp.next_node is not None and
-                   tmp.next_node.data < value):
+                    tmp.next_node.data < value):
                 tmp = tmp.next_node
-            tmp.next_node = new
             new.next_node = tmp.next_node
+            tmp.next_node = new
 
     def __str__(self):
-
-        """Defines the print representation of a SinglyLinkedList"""
-
-        lst = []
+        """Define the print() representation of a SinglyLinkedList."""
+        values = []
         tmp = self.__head
         while tmp is not None:
-            lst.append(str(tmp.data))
+            values.append(str(tmp.data))
             tmp = tmp.next_node
-        return '\n'.join(lst)
+        return ('\n'.join(values))
