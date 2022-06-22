66b7d5e464505df3c52bf61e7576b3564aebed56
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-21 19:16:33 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,7 @@ class Square:
 
     def my_print(self):
         if (self.__size == 0):
-            print()
+            print("")
             return
 
         [print("") for i in range(0, self.__position[1])]
