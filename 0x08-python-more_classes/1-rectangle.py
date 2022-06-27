d1b535ecdb179c1336300cd391de12c105e13de6
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-27 10:25:53 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
             width(int): the width of the rectangle default to 0
             height(int): the height of the rectangle default to 0
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
