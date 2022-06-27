7f5dd5af955722b8b35eec43161a86291256882d
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-27 09:57:37 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module defines a Rectangle"""
+
+
+class Rectangle:
+    """Defines the representation of a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,4 @@
+Description:
+
+0-rectangle.py: Program creates an empty class that defines a rectangle
+
