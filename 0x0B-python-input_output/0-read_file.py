cf958ee2e40eb6e99b52a5cb3d3c5209d0e3ac6a
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-07-05 06:43:27 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 0-read_file"""
+
+
+def read_file(filename=""):
+    """functio read a file and prints it
+
+    :param filename: path to file to be read
+    :return: Always void.
+
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,4 @@
+Description:
+
+0-read_file.py: Program reads a text file (UTF8) and prints it to stdout:
+
