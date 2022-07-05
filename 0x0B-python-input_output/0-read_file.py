15993d16ae7736100dc6c73be766ba21a25964ff
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-07-05 06:48:37 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,4 @@ def read_file(filename=""):
 
     """
     with open(filename, "r", encoding="utf-8") as file:
-        for line in file:
-            print(line)
+        print(file.read(), end="")
