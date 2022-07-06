7c19e55630e085d0a176f6827d4cf9253c7bf2d9
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-07-06 08:28:00 -0700

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,7 +2,7 @@
 """9-rectangle Module"""
 
 
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+BaseGeometry = __import__("6-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -18,3 +18,9 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
