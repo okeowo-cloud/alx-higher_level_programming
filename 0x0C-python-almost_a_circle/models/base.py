47ada70df95cb7c48383e6423d5828c8fcc6dd34
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-07-12 11:10:07 -0700

    ~

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,8 +34,7 @@ class Base:
         """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        if all(type(d) == "dict" for d in list_dictionaries):
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
