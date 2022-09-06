763a5137d0585588849334b134b08d3e2931848d
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-09-06 14:51:22 +0000

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) [this.width, this.height] = [w, h];
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let a = '';
+      for (let j = 0; j < this.width; j++) a += 'X';
+      console.log(a);
+    }
+  }
+};
