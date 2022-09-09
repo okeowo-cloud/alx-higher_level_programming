d768a862db39088e830cd23275e0651cec41c426
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-09-09 07:02:51 +0000

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""2-my_filter_states Module"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = "SELECT * FROM `states` WHERE `name` = '{}'".format(sys.argv[4])
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    [print(row) for row in rows]
