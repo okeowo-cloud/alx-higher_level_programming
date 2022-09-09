8d90696924415965498daf0750d52470450f887c
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-09-09 07:14:26 +0000

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,6 +8,6 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `cities`")
+    cursor.execute("SELECT * FROM `cities` ORDER BY id ASC;")
     rows = cursor.fetchall()
     [print(row) for row in rows]
