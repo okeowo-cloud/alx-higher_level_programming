14b52eddee390879fdfd2588426678397be1cf11
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-09-09 07:37:23 +0000

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,10 +6,11 @@ import sys
 
 
 if __name__ == "__main__":
+    name = sys.argv[4]
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM `cities` WHERE `state_id` =\
-                    (SELECT id FROM `states` WHERE `name` = %s)", (sys.argv[4],))
+                    (SELECT id FROM `states` WHERE `name` = %s)", (name,))
     rows = cursor.fetchall()
     for i in range(len(rows)):
         a, b, c = rows[i]
