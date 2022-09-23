95c8d79e543e9fec153bd1b383b6d8513031c66f
Tunde Okeowo <Okeowosamuel58.to@gmail.com>
2022-09-23 15:59:37 -0700

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Post
-# variable 'email' = 'test@gmail.com'
-# variable 'subject' = 'I will always be here for PLD'
 curl -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
