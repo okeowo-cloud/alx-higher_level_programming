f87ca4ee05105450e171ae20da5b1df208915442
Tunde Okeowo <Okeowosamuel58.to@gmail.com>
2022-09-23 17:08:48 -0700

    Bulk commit

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""2-post_email Module"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    url = sys.argv[1]
+    values = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print("{}".format(response.read().decode("utf-8")))
